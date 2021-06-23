cfb0bbca8d914d05bf85aad1528c7a969263291c
mstrekh <mstrekh@mail.ru>
2021-06-24 00:59:23 +0300

    Task is finished

--- 13_11_practice/init.js
+++ 13_11_practice/init.js
@@ -1,9 +1,29 @@
 
-window.onload = function() {
+document.getElementById('btnGenerate').addEventListener('click', () => {
     const initPerson = personGenerator.getPerson();
-    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
     document.getElementById('surnameOutput').innerText = initPerson.surName;
+    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('secondNameOutput').innerText = initPerson.secondName;
+    
     document.getElementById('genderOutput').innerText = initPerson.gender;
-    document.getElementById('birthYearOutput').innerText = initPerson.dateOfBirth; 
 
-};
+    document.getElementById('birthDayOutput').innerText = initPerson.dayOfBirth;
+    document.getElementById('birthMonthOutput').innerText = initPerson.monthOfBirth;
+    document.getElementById('birthYearOutput').innerText = initPerson.yearOfBirth;
+    
+    document.getElementById('occupationOutput').innerText = initPerson.occupation;
+})
+
+document.getElementById('btnClear').addEventListener('click', () => {
+    document.getElementById('surnameOutput').innerText = 'Фамилия';
+    document.getElementById('firstNameOutput').innerText = 'Имя';
+    document.getElementById('secondNameOutput').innerText = 'Отчество';
+    
+    document.getElementById('genderOutput').innerText = 'Пол';
+
+    document.getElementById('birthDayOutput').innerText = '';
+    document.getElementById('birthMonthOutput').innerText = '';
+    document.getElementById('birthYearOutput').innerText = 'Дата рождения';
+    
+    document.getElementById('occupationOutput').innerText = 'Профессия';  
+})
--- 13_11_practice/personGenerator.js
+++ 13_11_practice/personGenerator.js
@@ -35,10 +35,11 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+
     firstNameFemaleJson: `{
         "count": 10,
         "list": {     
-            "id_1": "Дарья ",
+            "id_1": "Дарья",
             "id_2": "Инна",
             "id_3": "Валентина",
             "id_4": "Анна",
@@ -50,55 +51,125 @@ const personGenerator = {
             "id_10": "Марина"
         }
     }`,
+    
+    secondNameJson: `{
+        "count": 4,
+        "list": {     
+            "id_1": "Александр",
+            "id_2": "Максим",
+            "id_3": "Иван",
+            "id_4": "Артем"
+        }
+    }`,
+
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
 
-    randomIntNumber: (max = 1, min = 0) => Math.floor(Math.random() * (max - min + 1) + min),
+    OccupationMaleJson: `{
+        "count": 5,
+        "list": {
+            "id_1": "Шахтер",
+            "id_2": "Слесарь",
+            "id_3": "Электрик",
+            "id_4": "Столяр",
+            "id_5": "Крановщик"
+        }
+    }`,
 
+    OccupationFemaleJson: `{
+        "count": 5,
+        "list": {
+            "id_1": "Медсестра",
+            "id_2": "Визажист",
+            "id_3": "Няня",
+            "id_4": "Дизайнер",
+            "id_5": "Кассир"
+        }
+    }`,
+
+    birthMonthJson: `{
+        "count": 12,
+        "list": {     
+            "id_1": "Января",
+            "id_2": "Февраля",
+            "id_3": "Марта",
+            "id_4": "Апреля",
+            "id_5": "Мая",
+            "id_6": "Июня",
+            "id_7": "Июля",
+            "id_8": "Августа",
+            "id_9": "Сентября",
+            "id_10": "Октября",
+            "id_11": "Ноября",
+            "id_12": "Декабря"
+        }
+    }`,
+    
+    randomIntNumber: (max = 1, min = 0) => Math.floor(Math.random() * (max - min + 1) + min),
+    
     randomValue: function (json) {
         const obj = JSON.parse(json);
         const prop = `id_${this.randomIntNumber(obj.count, 1)}`;  // this = personGenerator
         return obj.list[prop];
     },
-
-    randomFirstName: function(gender) {
-        if(gender==='Мужчина') {
-            return this.randomValue(this.firstNameMaleJson);
-        }
-        else {
-            return this.randomValue(this.firstNameFemaleJson);
-        }
-    },
-
-
-     randomSurname: function(gender) {
-        if(gender==='Мужчина') {
-            return this.randomValue(this.surnameJson);
-        }
-        else {
-            return `${this.randomValue(this.surnameJson)}а`;
-        }
-
-    },
+    
     randomGender: function() {
         const gender = this.randomIntNumber();
-        if (gender) {
-            return this.GENDER_MALE
-         }
-         else {
-             return this.GENDER_FEMALE;
-        } 
-             
+            return gender ? this.GENDER_MALE : this.GENDER_FEMALE;
+    },
+
+    randomFirstName: function(gender) {
+        return gender==='Мужчина' ? this.randomValue(this.firstNameMaleJson) : this.randomValue(this.firstNameFemaleJson);
+    },
+    
+    randomSecondName: function(gender) {
+        return gender==='Мужчина' ? `${this.randomValue(this.secondNameJson)}ович` : `${this.randomValue(this.secondNameJson)}овна`;
+    },
+    
+    randomSurname: function(gender) {
+        return gender==='Мужчина' ? this.randomValue(this.surnameJson) : `${this.randomValue(this.surnameJson)}а`;
+    },
+
+    randomDayOfBirth: function(monthOfBirth) {
+        let dayOfBirth = this.randomIntNumber(1, 31); 
+        if (monthOfBirth =='Апреля' 
+        || monthOfBirth =='Июня' 
+        || monthOfBirth =='Сентября' 
+        || monthOfBirth =='Ноября'  
+        && dayOfBirth === 31 ) {
+            dayOfBirth = 30;
+        }
+        else {
+            if(monthOfBirth === 'Февраля' && dayOfBirth > 28) {
+                dayOfBirth = 28;
+            }
+        }
+
+        return dayOfBirth;
+    },
+
+    randomMonthOfBirth: function() {
+        return this.randomValue(this.birthMonthJson);
+    },
+
+    randomOccupation: function (gender) {
+        return gender==='Мужчина' ? this.randomValue(this.OccupationMaleJson) : this.randomValue(this.OccupationFemaleJson);
     },
 
     getPerson: function () {
         this.person = {};
         
         this.person.gender = this.randomGender();
+        
         this.person.firstName = this.randomFirstName(this.person.gender);
+        this.person.secondName = this.randomSecondName(this.person.gender);
         this.person.surName = this.randomSurname(this.person.gender);
-        this.person.dateOfBirth = `${this.randomIntNumber(1950, 2000)} года рождения`;
 
+        this.person.monthOfBirth = this.randomMonthOfBirth();
+        this.person.dayOfBirth = this.randomDayOfBirth(this.person.monthOfBirth);
+        this.person.yearOfBirth = `${this.randomIntNumber(1950, 2000)}`;
+        
+        this.person.occupation = this.randomOccupation(this.person.gender);
         return this.person;
     }
 };
